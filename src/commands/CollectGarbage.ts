f397f382f2d09fa58ebb6df8b069e66a91813335
Deskehs <124361319+Deskehs@users.noreply.github.com>
2024-08-16 04:12:05 +0800

    rewrite prepare

--- src/commands/CollectGarbage.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Implement stubbed commands
-
-function CollectGarbage(response_handler: any, request: any, updater: any) {
-    updater.isRunningUpdate = true;
-    response_handler(JSON.stringify([request[0], "Ok"]));
-  }
-  
-  export { CollectGarbage };
--- src/node.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-    export interface Process {
-      noAsar: boolean;
-    }
-  }
-  
